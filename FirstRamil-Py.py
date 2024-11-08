f39e1773d02178b46d8c0205c07ecbd7f25b65a1
Ramil-AQA <ramilgrc2004@gmail.com>
2024-11-08 22:53:00 +0500

    First_theme_meme

--- FirstRamil-Py.py
+++ FirstRamil-Py.py
@@ -1,4 +1,5 @@
-order  = {
+from datetime import datetime
+order = {
     "state": 0,
     "data": [
         {
@@ -7,7 +8,7 @@ order  = {
             "brand_id": 84375,
             "delay": 1,
             "startedAt": "2024-03-21T16:48:03.513Z",
-            "completedAt": "2024-03-21T16:48:03.513Z",
+            "completedAt": "2024-03-21T21:48:03.513Z",
             "completed": 0,
             "wait_refund": 0,
             "refunded": 0
@@ -18,7 +19,7 @@ order  = {
             "brand_id": 88339,
             "delay": 2,
             "startedAt": "2024-03-21T16:27:32.062Z",
-            "completedAt": "2024-03-21T16:28:32.062Z",
+            "completedAt": "2024-03-21T21:28:32.062Z",
             "completed": 0,
             "wait_refund": 2,
             "refunded": 0
@@ -28,69 +29,33 @@ order  = {
             "count": 16,
             "brand_id": 88339,
             "delay": 3,
-            "startedAt": "2024-03-21T16:17:04.723Z",
-            "completedAt": "2024-03-21T16:17:04.723Z",
+            "startedAt": "2024-03-01T16:17:04.723Z",
+            "completedAt": "2024-03-22T22:17:04.723Z2",
             "completed": 7,
             "wait_refund": 3,
             "refunded": 6
         }
     ]
-    }
-### Словарь; массивы, объекты
-Operator = {
-    'name' : 'Ramil',
-    'mail' : 'ramilgrc2004@gmail.com'
 }
+### 1 задание : Надо убедиться, что заказы вообще есть в ответе от сервера
+assert len(order["data"]) > 0, "Заказы отсутствуют"
+### конец первого задания
+### 2 задание: Надо убедиться, что время выполнение первого и второго заказов не превышает
+started_1 = datetime.strptime(order["data"][0]["startedAt"], "%Y-%m-%dT%H:%M:%S.%fZ")
+completed_1 = datetime.strptime(order["data"][0]["completedAt"], "%Y-%m-%dT%H:%M:%S.%fZ")
+difference_time_0 = (completed_1 - started_1).seconds
+print(difference_time_0)
+started_2 = datetime.strptime(order["data"][1]["startedAt"], "%Y-%m-%dT%H:%M:%S.%fZ")
+completed_2 = datetime.strptime(order["data"][1]["completedAt"], "%Y-%m-%dT%H:%M:%S.%fZ")
+difference_time_1 = (completed_2 - started_2).seconds
+print(difference_time_1)
 
-ID_one = order["data"][0]["brand_id"]
-ID_two = order["data"][1]["brand_id"]
-ID_three = order["data"][2]["brand_id"]
-mass = [ID_one, ID_two, ID_three]
-print(mass)
-Obj_dictionary = {
-   'completed' : {
-  'one' : order["data"][0]["completed"],
-    'two' :   order["data"][1]["completed"],
-       'three'  :   order["data"][2]["completed"]
-},
-    'wait_refund' : {
-        'one': order["data"][0]["wait_refund"],
-        'two': order["data"][1]["wait_refund"],
-        'three': order["data"][2]["wait_refund"]
-    },
-    'refunded' :   {
-        'one': order["data"][0]["refunded"],
-        'two': order["data"][1]["refunded"],
-        'three': order["data"][2]["refunded"]
-}
-}
-print(Obj_dictionary)
-### Конец словарика
-if order == " " :
-        print('status : ERROR')
-else :
-    print("status : OK")
-result_time =  order["data"][0]["delay"] + order["data"][1]["delay"]
-if result_time >= 6 :
-    print("is not good <<<ERROR>>>")
-else :
-    print("status: OK")
-if order == None :
-    print ("is not good <<<ERROR>>>")
-else :
-    print("status: OK")
-if len(order['data']) < 0 :
-    print ("is not good <<<ERROR>>>")
-else :
-    print("status: OK")
-Third_date1 = order["data"][2]["completed"]
-Third_date2 = order["data"][2]["wait_refund"]
-Third_date3 = order["data"][2]["refunded"]
-if Third_date1 < order["data"][2]["count"] / 2 :
-    print ("OK")
-elif Third_date3 < Third_date1 :
-    print ("OK")
-elif  Third_date2  <= Third_date3 :
-    print("OK")
-else :
-    print("ERROR")
+time = "%Y-%m-%dT%H:%M:%S.%fZ"
+result = 0
+for i in range(2):
+    final_completed = datetime.strptime(order["data"][i]["startedAt"], time)
+final_started = datetime.strptime(order["data"][i]["startedAt"], time)
+result += (final_completed - final_started).seconds
+assert result < 6, "время превышает 6 часов"
+print("время равна ", result)
+### 3-е задание:
