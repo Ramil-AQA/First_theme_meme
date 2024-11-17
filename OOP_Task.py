6d5ae276bd2a3d1f6eace596b48300d6fa75721e
Ramil-AQA <ramilgrc2004@gmail.com>
2024-11-17 18:39:50 +0500

    First_theme_meme изменение недочетов, добавление экземпляров

--- OOP_Task.py
+++ OOP_Task.py
@@ -4,39 +4,44 @@ class BankAccount:
         self.owner = owner
         self.__balance = balance
 
-        def deposit(self, amount):
-            if amount > 0:
-                self.balance += amount
-                print(f'добавлена сумма {amount}')
-            else:
-                print(f'ValueError: неккоректная сумма {amount}')
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+            print(f'добавлена сумма {amount}')
+        else:
+            print(f'неккоректная сумма депозита: {amount}')
 
-                def withdraw(self, amount):
-                    if self.balance < amount:
-                        print("Ошибка, недостаточно средств")
-                    else:
-                        self.balance -= amount
-                        print(f"Снятие баланса, остаток на счете: {self.balance}")
+    def withdraw(self, amount):
+        if self.__balance < amount:
+            print("Ошибка, недостаточно средств")
+        else:
+            self.__balance -= amount
+            print(f"Снятие баланса, остаток на счете: {self.__balance}")
 
-                        def get_balance(self):
-                            print(f"Текущий баланс {self.balance}")
+            def get_balance(self):
+                print(f"Текущий баланс {self.__balance}")
 
 
 # Создание класса SavingsAccount
 class SavingsAccount(BankAccount):
-    def __init__(self, owner, interest_rate=0.05, balance=0):
+    def __init__(self, owner, balance=0):
         super().__init__(owner, balance)
+        self.interest_rate = 0.05
 
-        def apply_interest(self):
-            self.__balance *= interest_rate
-            return self.balance
+    def apply_interest(self):
+        self._BankAccount__balance *= self.interest_rate
+        print(f"Начислен процент {self.interest_rate} сумма баланса равна: {self._BankAccount__balance}")
 
-            # Создание класса CheckingAccount (наследуется от BankAccount):
-            class CheckingAccount(BankAccount):
+        # Создание класса CheckingAccount (наследуется от BankAccount):
 
-                def withdraw(self, amount):
-                    if self.__balance < amount:
-                        print("Ошибка, недостаточно средств")
-                    else:
-                        self.__balance -= amount
-                        print(f"Снятие баланса, остаток на счете: {self.balance}")
+
+class CheckingAccount(BankAccount):
+    def withdraw(self, amount):
+        self.__balance -= amount
+        print(f"Снятие баланса, остаток на счете: {self.__balance}")
+
+
+my_account = SavingsAccount(owner="Timur")
+my_account.deposit(190)
+my_account.withdraw(100)
+my_account.apply_interest()
