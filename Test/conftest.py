492752d6adfd056c5ff58e34d38e1706a8f29568
Ramil-AQA <ramilgrc2004@gmail.com>
2024-11-24 15:58:21 +0500

    правки

--- Test/conftest.py
+++ Test/conftest.py
@@ -1,18 +1,18 @@
 import pytest
 import requests
 from faker import Faker
+from constant import BASE_URL
+from constant import HEADERS
 
 fake = Faker()
-BASE_URL = "https://restful-booker.herokuapp.com"
-HEADERS = {"Content-Type": "application/json", "Accept": "application/json"}
 
 
 @pytest.fixture(scope="session")
 # авторизация
-def auth_token():
+def auth_session():
     session = requests.Session()
     session.headers.update(HEADERS)
-    response = requests.post(f"{BASE_URL}/auth", headers=HEADERS,
+    response = session.post(f"{BASE_URL}/auth", headers=HEADERS,
                              json={"username": "admin", "password": "password123"})
     print(response.status_code)
     assert response.status_code == 200, "Ошибка авторизации, статус не 200"
