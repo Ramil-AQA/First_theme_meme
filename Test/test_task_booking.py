58d19225a9d38ef52c7f06416914ae4e9317b106
Ramil-AQA <ramilgrc2004@gmail.com>
2024-11-24 14:09:42 +0500

    херня в прошлом была, поправил переменные

--- Test/test_task_booking.py
+++ Test/test_task_booking.py
@@ -1,19 +1,13 @@
-from venv import create
-
 import requests
-import conftest
-from constant import BASE_URL
-from constant import HEADERS
-
-
-
-
+BASE_URL = "https://restful-booker.herokuapp.com"
+HEADERS = {"Content-Type": "application/json", "Accept": "application/json"}
 
 
 class TestBooker:
 
     # создание бронирования
-    def test_create_method(self, booking_data, get_token):
-        create_booking = requests.get_token.post(f"{self.BASE_URL}/booking", headers=self.HEADERS, json=booking_data)
+    def test_create_method(self, booking_data, auth_token):
+        create_booking = auth_token.post(f"{BASE_URL}/booking", headers=HEADERS, json=booking_data)
         assert create_booking.response.status == 200
         booking_id = create_booking.json.get("bookingid")
+        assert booking_id is not None, "ID букинга нет в ответе"
