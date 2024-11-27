3eb75ee6b782c6bbabc882420461cef3920c68a7
Ramil-AQA <ramilgrc2004@gmail.com>
2024-11-27 21:30:16 +0500

    так же внес пару фикстур

--- Test/conftest.py
+++ Test/conftest.py
@@ -1,6 +1,10 @@
+import random
+import string
+
 import pytest
 import requests
 from faker import Faker
+
 from constant import BASE_URL
 from constant import HEADERS
 
@@ -13,7 +17,7 @@ def auth_session():
     session = requests.Session()
     session.headers.update(HEADERS)
     response = session.post(f"{BASE_URL}/auth", headers=HEADERS,
-                             json={"username": "admin", "password": "password123"})
+                            json={"username": "admin", "password": "password123"})
     print(response.status_code)
     assert response.status_code == 200, "Ошибка авторизации, статус не 200"
     token = response.json().get("token")
@@ -25,14 +29,67 @@ def auth_session():
 @pytest.fixture()
 # генерация данных
 def booking_data():
+    def _booking_data():
+        return {
+            "firstname": fake.first_name(),
+            "lastname": fake.last_name(),
+            "totalprice": fake.random_int(100, 1000000),
+            "depositpaid": True,
+            "bookingdates": {
+                "checkin": "2021-01-01",
+                "checkout": "2023-01-01"
+            },
+            "additionalneeds": "Breakfast"
+        }
+
+    return _booking_data()
+
+
+@pytest.fixture()
+def update_data():
     return {
         "firstname": fake.first_name(),
         "lastname": fake.last_name(),
         "totalprice": fake.random_int(100, 1000000),
-        "depositpaid": True,
+        "depositpaid": bool(random.getrandbits(1)),
         "bookingdates": {
-            "checkin": "2021-01-01",
-            "checkout": "2023-01-01"
+            "checkin": "2020-01-01",
+            "checkout": "2024-01-01"
         },
         "additionalneeds": "Breakfast"
     }
+
+
+@pytest.fixture()
+def values_for_patch():
+    return {
+        "firstname": fake.first_name(),
+        "lastname": fake.last_name(),
+    }
+
+
+@pytest.fixture()
+def negative_values_for_patch():
+    return 0
+
+
+@pytest.fixture()
+def empty_data():
+    return {
+        "firstname": None,
+        "lastname": None,
+        "totalprice": [],
+        "depositpaid": "",
+        "bookingdates": {
+            "checkin": None,
+            "checkout": None
+        },
+        "additionalneeds": None
+    }
+
+
+@pytest.fixture()
+def invalid_values(length=112):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    values = ''.join(random.choice(characters) for _ in range(length))
+    return values
